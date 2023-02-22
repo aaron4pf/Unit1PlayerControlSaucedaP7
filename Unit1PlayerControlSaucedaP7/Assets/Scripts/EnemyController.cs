8f8609924c3aea81f61bb91a4304dc218ec4b2cb
aaron4pf <aaronsauceda12@gmail.com>
2023-02-22 15:28:44 -0800

    finisahhed bonus

--- Unit1PlayerControlSaucedaP7/Assets/Scripts/EnemyController.cs
+++ Unit1PlayerControlSaucedaP7/Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private float speed = 20.0f;
+    public float speed = 20.0f;
 
     // Start is called before the first frame update
     void Start()
--- Unit1PlayerControlSaucedaP7/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControlSaucedaP7/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera firstPerson;
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +27,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardInput);
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up,turnSpeed*horizontalInput*Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            firstPerson.enabled = !firstPerson.enabled;
+        }
     }
 } 
